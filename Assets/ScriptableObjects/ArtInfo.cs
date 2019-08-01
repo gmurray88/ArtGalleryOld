aba82a046f5d5e5092213a8fe8d6fe8d4b3d7703
Glenn Murray <evocativeu@icloud.com>
2019-08-01 13:50:22 -0700

    First commit

---
+++ Assets/ScriptableObjects/ArtInfo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+[CreateAssetMenu(menuName = "My Objects/Art Info")]
+public class ArtInfo : ScriptableObject
+{
+    public Texture image;
+    public string title;
+    public string artist;
+    [Multiline]
+    public string description;
+}
